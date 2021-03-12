1f1c6490db4626a0f8b1c58d9ed02dbdea98074d
Batuhan Arık <batuhanarik123@gmail.com>
2021-03-12 21:50:29 +0300

    Cache,Transaction,Performance Aspectleri eklendi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -1,6 +1,8 @@
 ﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
 using Business.Constants;
 using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Caching;
 using Core.Aspects.Autofac.Validation;
 using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
@@ -25,7 +27,9 @@ namespace Business.Concrete
             _carDal = carDal;
         }
 
+        [SecuredOperation("car.add,admin")]
         [ValidationAspect(typeof(CarValidator))]
+        [CacheRemoveAspect("ICarService.Get")]
         public IResult Add(Car car)
         {
             _carDal.Add(car);
